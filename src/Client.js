7850364a177a81841f34ca79cd00bc260521b506
Sean Wilson <hello@imsean.me>
2017-01-16 23:13:55 -0600

    add query

--- src/Client.js
+++ src/Client.js
@@ -3,16 +3,24 @@ const superagent = require('superagent');
 const chatBotError = require('./Error');
 const Promise = require('bluebird');
 
-class chatBot {
+const API_URL = 'http://martmists.tk/api/chatbot/'
+
+class ChatBot {
     constructor (token) {
-        this.token = token;
+      this.token = token;
+    }
+
+    query (message) {
+      return new Promise((resolve, reject) => {
+        superagent
+          .get(API_URL)
+          .query({ token: this.token, query: message })
+          .end((err, res) {
+            if (err) return reject(err)
+            resolve(res)
+          })
+      })
     }
 }
 
-query (message) {
-    
-    //it looks so gay so this is empty for now
-    
-}
-
-module.exports = chatBotClient;
+module.exports = ChatBot;
